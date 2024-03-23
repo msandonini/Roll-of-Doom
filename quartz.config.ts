efc5e009df994be9fe9fc6f5d27ae3c2d3054f6f
Mirco Sandonini <mirco.ms16@gmail.com>
2024-03-23 21:12:08 +0100

    Quartz sync: Mar 23, 2024, 9:12 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Roll of Doom",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "it-IT",
+    baseUrl: "msandonini.github.io/Roll-of-Doom",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
